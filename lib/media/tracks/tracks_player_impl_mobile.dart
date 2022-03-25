847c34148dfda1eaa1e03f792b8a639029cc5d28
inkbox <enpassPixiv@protonmail.com>
2022-03-25 13:29:31 +0800

    fix: 修复移动端传递的播放url和图片url

--- lib/media/tracks/tracks_player_impl_mobile.dart
+++ lib/media/tracks/tracks_player_impl_mobile.dart
@@ -17,11 +17,15 @@ import 'package:quiet/extension.dart';
 
 extension _Track on Track {
   player.Music toMusic() {
+    var i = imageUrl;
+    if (!NetworkSingleton.instance.allowNetwork()) {
+      i = null;
+    } else if (imageUrl != null && imageUrl!.isEmpty) {
+      i = null;
+    }
+
     return player.Music(
-        url: file ?? (uri ?? ''),
-        artist: '',
-        title: name,
-        image: NetworkSingleton.instance.allowNetwork() ? (imageUrl) : null);
+        url: file ?? (mp3Url ?? ''), artist: '', title: name, image: i);
   }
 }
 
@@ -31,38 +35,51 @@ class TracksPlayerImplMobile extends TracksPlayer {
     // _player.playbackStateListenable.addListener(notifyPlayStateChanged);
     _player = player.MusicPlayer(
         onCompleted: () {
+          log('onCompleted');
           _isPlaying = false;
           notifyPlayStateChanged();
         },
         onDuration: (duration) {
+          log('onDuration');
+
           _isBuffing = false;
           _duration = duration;
           notifyPlayStateChanged();
         },
         onLoading: () {
+          log('onLoading');
           _isBuffing = true;
           notifyPlayStateChanged();
         },
         onPlaying: () {
+          log('onPlaying');
           _isPlaying = true;
           _isBuffing = false;
           notifyPlayStateChanged();
         },
         onPlayNext: () => skipToNext(),
         onError: (e) {
+          log('onError');
+
           _error = true;
           notifyPlayStateChanged();
         },
         onPaused: () {
+          log('onPaused');
+
           _isPlaying = false;
           notifyPlayStateChanged();
         },
         onPlayPrevious: () => skipToPrevious(),
         onPosition: (p) {
+          log('onPosition');
+
           _position = p;
           notifyPlayStateChanged();
         },
         onStopped: () {
+          log('onStopped');
+
           _isPlaying = false;
           notifyPlayStateChanged();
         });
@@ -104,19 +121,20 @@ class TracksPlayerImplMobile extends TracksPlayer {
 
   Future<void> _play(Track? value) {
     if (value != null) {
-      if (value.mp3Url == null) {
+      // if (value.mp3Url == null || value.mp3Url!.isEmpty) {
         return networkRepository!.getPlayUrl(value).then((v) {
           value.mp3Url = v.mp3Url;
+          log('播放器播放的url=${v.mp3Url.toString()} file=${v.file} ${value.toMusic()}');
           _current = value;
           _player!.play(value.toMusic(), showNext: true, showPrevious: true);
           notifyPlayStateChanged();
         });
-      } else {
-        _current = value;
-        return _player!
-            .play(value.toMusic(), showNext: true, showPrevious: true)
-            .then((value) => notifyPlayStateChanged());
-      }
+      // } else {
+      //   _current = value;
+      //   return _player!
+      //       .play(value.toMusic(), showNext: true, showPrevious: true)
+      //       .then((value) => notifyPlayStateChanged());
+      // }
     }
     return Future.error(const QuietException('no track'));
   }
@@ -141,11 +159,9 @@ class TracksPlayerImplMobile extends TracksPlayer {
   Future<void> playFromMediaId(int trackId) async {
     // await _player.transportControls.playFromMediaId(trackId.toString());
     final track = trackList.tracks.firstWhereOrNull((t) => t.id == trackId);
+    _play(track);
     if (track != null) {
-      MusicApiContainer.instance
-          .getApi(track.origin)
-          .then((value) => value.playUrl(track))
-          .then((value) => _play(value));
+      _play(track);
     } else {
       log('没有track=$trackId list=${trackList.tracks}');
     }
