26e7ebd4bc5727b78d43a783a94d31ed6dc51175
inkbox <enpassPixiv@protonmail.com>
2023-01-14 19:50:40 +0800

    wip: 通知栏唤起app

---
+++ mobile_player/android/src/main/java/com/salkuadrat/music/NotificationClickReceiver.java
@@ -0,0 +1,16 @@
+package com.salkuadrat.music;
+import io.flutter.embedding.android.FlutterActivity;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+import android.content.BroadcastReceiver;
+public class NotificationClickReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //todo 跳转之前要处理的逻辑
+        Log.i("TAG", "userClick:我被点击啦！！！ ");
+        Intent newIntent = new Intent(context, FlutterActivity.class);
+        context.startActivity(newIntent);
+    }
+}
