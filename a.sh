28f61e3d97cd9bac46c5efba53797e6b0efafea3
inkbox <enpassPixiv@protonmail.com>
2023-08-02 14:20:37 +0800

    build: 兼容flutter 3.10.6 android构建

--- a.sh
+++ a.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set -e
+
 ## 处理更新信息
 sed -i '4c     version: '"$1" pubspec.yaml
 
 
-# 兼容 flutter 3.10.5
-test -d "/opt/hostedtoolcache/flutter/" && ( sed -i '39 d' "/opt/hostedtoolcache/flutter/stable-3.10.5-x64/.pub-cache/hosted/pub.dev/connectivity_plus-4.0.1/android/build.gradle" &&   sed -i '27 d' "/opt/hostedtoolcache/flutter/stable-3.10.5-x64/.pub-cache/hosted/pub.dev/connectivity_plus-4.0.1/android/build.gradle" &&   sed -i '49 d' "/opt/hostedtoolcache/flutter/stable-3.10.5-x64/.pub-cache/hosted/pub.dev/audioplayers_android-3.0.2/android/build.gradle" &&   sed -i '41 d' "/opt/hostedtoolcache/flutter/stable-3.10.5-x64/.pub-cache/hosted/pub.dev/file_picker-5.3.2/android/build.gradle" &&  sed -i '31 d' "/opt/hostedtoolcache/flutter/stable-3.10.5-x64/.pub-cache/hosted/pub.dev/package_info_plus-4.0.2/android/build.gradle" )
+flutter_path="/opt/hostedtoolcache/flutter/stable-3.10.6-x64/.pub-cache/hosted/pub.dev"
+
+# 兼容 flutter 3.10.6
+test -d "/opt/hostedtoolcache/flutter/" && ( sed -i '39 d' "$flutter_path/connectivity_plus-4.0.1/android/build.gradle" &&   sed -i '27 d' "$flutter_path/connectivity_plus-4.0.1/android/build.gradle" &&   sed -i '49 d' "$flutter_path/audioplayers_android-3.0.2/android/build.gradle"  &&   sed -i '41 d' "$flutter_path/file_picker-5.3.3/android/build.gradle" &&  sed -i '31 d' "$flutter_path/package_info_plus-4.0.2/android/build.gradle" )
 
 ## linxu deb打包 ./deb.sh version
 flutter build apk --split-per-abi --release -vv
