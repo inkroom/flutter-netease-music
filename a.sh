d93f9d4ecefb24ea01250e32d9c9bf8c6f7e1ae5
inkbox <enpassPixiv@protonmail.com>
2023-10-10 11:22:53 +0800

    build: 升级action linux系统版本

--- a.sh
+++ a.sh
@@ -6,7 +6,7 @@ set -e
 sed -i '4c     version: '"$1" pubspec.yaml
 
 
-flutter_path="/opt/hostedtoolcache/flutter/stable-3.10.6-x64/.pub-cache/hosted/pub.dev"
+flutter_path="$PUB_CACHE/hosted/pub.dev"
 
 # 兼容 flutter 3.10.6
 test -d "/opt/hostedtoolcache/flutter/" && ( sed -i '39 d' "$flutter_path/connectivity_plus-4.0.1/android/build.gradle" &&   sed -i '27 d' "$flutter_path/connectivity_plus-4.0.1/android/build.gradle" &&   sed -i '49 d' "$flutter_path/audioplayers_android-3.0.2/android/build.gradle"  &&   sed -i '41 d' "$flutter_path/file_picker-5.3.3/android/build.gradle" &&  sed -i '31 d' "$flutter_path/package_info_plus-4.0.2/android/build.gradle" )
