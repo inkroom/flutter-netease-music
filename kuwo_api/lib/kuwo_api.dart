51a909d33b7679fe7bbd15ff089415cb51a55c1a
inkbox <panxiaoyu@xiyoumail.com>
2024-11-12 17:45:29 +0800

    fix: 更换酷我api接口

--- kuwo_api/lib/kuwo_api.dart
+++ kuwo_api/lib/kuwo_api.dart
@@ -1,12 +1,16 @@
 import 'dart:convert';
-import 'dart:developer';
+import 'dart:developer' as dev;
 import 'dart:io';
+import 'dart:math';
 
 import 'package:track_music_api/track_music_api.dart';
 
-/// 基础接口
+const COOKIE_NAME = "Hm_Iuvt_cdb524f42f23cer9b268564v7y735ewrq2324";
+
 class KuWoApi extends MusicApi {
   KuWoApi(String dir);
+
+  String? cookie;
 
   /// 转码
   String _escape2Html(String str) {
@@ -19,17 +23,11 @@ class KuWoApi extends MusicApi {
 
   @override
   Future<Track> playUrl(Track track) {
-    log('hash=${track.extra}');
-
     return _doRequest(
-            'http://www.kuwo.cn/api/v1/www/music/playUrl?mid=${track.id}&type=music&httpsStatus=1',
+            'https://www.kuwo.cn/api/v1/www/music/playUrl?mid=${track.id}&type=music&httpsStatus=1',
             {
-              'Referer': 'http://www.kuwo.cn/play_detail/${track.id}',
-              'Cookie':
-                  'Hm_lvt_cdb524f42f0ce19b169a8071123a4797=1691246796; _ga=GA1.2.1036361046.1691246796; _gid=GA1.2.1524982465.1691246796; Hm_lpvt_cdb524f42f0ce19b169a8071123a4797=1691250747; _gat=1; Hm_Iuvt_cdb524f42f0ce19b169b8072123a4727=CYcsc7PzFx3PTnkYht2K2QGH4XNAabjE; _ga_ETPBRPM9ML=GS1.2.1691250748.2.1.1691250767.41.0.0',
-              'csrf': 'CUQ9R4J2L4',
-              'Secret':
-                  '1d05191f99b3bd73f843de10f088f2a455c1cbc38fbc7bc34c244290c5db7d7e02caa302',
+              "Secret": _getSecret(cookie ?? "", COOKIE_NAME),
+              // 'Referer': 'http://www.kuwo.cn/play_detail/${track.id}',
               'User-Agent':
                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
               'Pragma': 'no-cache',
@@ -40,11 +38,26 @@ class KuWoApi extends MusicApi {
         .then((value) => value.transform(utf8.decoder).join())
         .then((value) => json.decode(value))
         .then((e) {
-      log('歌曲详情=$e');
+      dev.log('歌曲详情=$e');
       if (e['code'] != 200) {
-        return Future.error(PlayDetailException('获取歌曲详情失败', track));
+        if (e['msg'].toString().contains("付费")) {
+          return Future.error(PlayDetailException(
+              '付费歌曲',
+              Track(
+                  id: track.id,
+                  uri: track.uri,
+                  mp3Url: '',
+                  name: _escape2Html(track.name),
+                  artists: track.artists,
+                  album: track.album,
+                  imageUrl: track.imageUrl,
+                  duration: track.duration,
+                  type: TrackType.vip,
+                  extra: track.extra,
+                  origin: origin)));
+        }
+        return Future.error(PlayDetailException('获取歌曲详情失败 ', track));
       }
-      log('playUrl=${e['data']['url']}');
       return Track(
           id: track.id,
           uri: track.uri,
@@ -63,70 +76,47 @@ class KuWoApi extends MusicApi {
   @override
   Future<PageResult<Track>> search(String keyword, int page, int size) {
     return _doRequest(
-        "http://www.kuwo.cn/api/www/search/searchMusicBykeyWord?key=${Uri.encodeFull(keyword)}&pn=$page&rn=$size&httpsStatus=1",
-            {
-              'Cookie':
-                  'Hm_lvt_cdb524f42f0ce19b169a8071123a4797=1691246796; _ga=GA1.2.1036361046.1691246796; _gid=GA1.2.1524982465.1691246796; Hm_lpvt_cdb524f42f0ce19b169a8071123a4797=1691250747; _gat=1; Hm_Iuvt_cdb524f42f0ce19b169b8072123a4727=CYcsc7PzFx3PTnkYht2K2QGH4XNAabjE; _ga_ETPBRPM9ML=GS1.2.1691250748.2.1.1691250767.41.0.0',
-              'Referer': 'http://www.kuwo.cn',
-              'csrf': 'CUQ9R4J2L4',
-              'Secret':
-                  '1d05191f99b3bd73f843de10f088f2a455c1cbc38fbc7bc34c244290c5db7d7e02caa302',
-              'User-Agent':
-                  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
-              'Pragma': 'no-cache',
-              'Cache-Control': 'no-cache'
-            },
+            "https://www.kuwo.cn/search/searchMusicBykeyWord?vipver=1&client=kt&ft=music&cluster=0&strategy=2012&encoding=utf8&rformat=json&mobi=1&issubtitle=1&show_copyright_off=1&pn=${page - 1}}&rn=$size&all=${Uri.encodeFull(keyword)}",
+            {},
             {},
             'get')
-        .then((value) {
-          log('酷我音乐 = $value ${value.statusCode} ');
-          return value;
-        })
         .then((value) => value.transform(utf8.decoder).join())
-        .then((value) {
-          log('酷我音乐22 = $value  ');
-          return value;
-        })
         .then((value) => json.decode(value))
         .then((value) {
-          if (value['code'] != 200) {
-            return Future.error(SearchException('搜索失败'));
-          }
-          log('value=$value');
-          final total = int.parse(value['data']['total']);
-          List<Track> list = List.empty(growable: true);
-          final s = value['data']['list'] as List;
-          for (var e in s) {
-            list.add(Track(
-                id: int.parse(e['musicrid']
-                    .toString()
-                    .substring(e['musicrid'].toString().indexOf('_') + 1)),
-                uri: '',
-                name: _escape2Html(e['name']),
-                artists: [
-                  ArtistMini(
-                      id: e['artistid'], name: e['artist'], imageUrl: e['pic'])
-                ],
-                album: (e['albumid'] != null && e['albumid'] != '')
-                    ? AlbumMini(
-                        id: int.parse(e['albumid']),
-                        name: e['album'],
-                        picUri: "albumpic")
-                    : null,
-                imageUrl: '',
-                duration: Duration(seconds: e['duration']),
+      // if (value['abslist'] != 200) {
+      //   return Future.error(SearchException('搜索失败'));
+      // }
+      final total = int.parse(value['TOTAL']);
+      List<Track> list = List.empty(growable: true);
+      final s = value['abslist'] as List;
+      for (var e in s) {
+        list.add(Track(
+            id: int.parse(e['DC_TARGETID']),
+            uri: '',
+            name: (e['NAME']),
+            artists: [
+              ArtistMini(
+                  id: int.parse(e['ARTISTID']),
+                  name: e['ARTIST'],
+                  imageUrl: e['hts_MVPIC'])
+            ],
+            album: (e['ALBUMID'] != null && e['ALBUMID'] != '')
+                ? AlbumMini(
+                    id: int.parse(e['ALBUMID']),
+                    name: e['ALBUM'],
+                    picUri: e["hts_MVPIC"])
+                : null,
+            imageUrl: e["hts_MVPIC"],
+            duration: Duration(seconds: int.parse(e['DURATION'])),
 
-                /// 1111 为付费的
-                /// 1100和0000都能播放
-                type: e['payInfo']['play'].toString().endsWith("00")
-                    ? TrackType.free
-                    : TrackType.noCopyright,
-                origin: origin,
-                extra: e['musicrid']));
-          }
+            /// 搜索的时候无法判断是否可以播放，只能推迟到播放的时候
+            type: TrackType.free,
+            origin: origin,
+            extra: e['DC_TARGETID']));
+      }
 
-          return PageResult(data: list, total: total);
-        });
+      return PageResult(data: list, total: total);
+    });
   }
 
   @override
@@ -141,36 +131,149 @@ class KuWoApi extends MusicApi {
   @override
   String get icon => "assets/icon.ico";
 
+  String _formatDuration(Duration v) {
+    return v.inMinutes.toString().padLeft(2, '0') +
+        ":" +
+        v.inSeconds.toString().padLeft(2, '0') +
+        '.' +
+        v.inMilliseconds.toString().padRight(4, '0').substring(0, 3);
+  }
+
   @override
   Future<LyricContent?> lyric(Track track) {
-    return Future.error(LyricException(''));
-    // return _doRequest(
-    //         'https://wwwapi.kugou.com/yy/index.php?r=play%2Fgetdata&hash=${track.extra}&appid=1014&platid=4&album_id=${track.album?.id}',
-    //         {
-    //           'Cookie':
-    //               'kg_mid=c64d12df8bef9907d2c2b636167d10a8; kg_dfid=1tyJiN22XC5K3SD5Xz0ojfVx; kg_dfid_collect=d41d8cd98f00b204e9800998ecf8427e'
-    //         },
-    //         {},
-    //         'get')
-    //     .then((value) => value.transform(utf8.decoder).join())
-    //     .then((value) => json.decode(value))
-    //     .then((e) {
-    //   log('歌词详情==$e');
-    //   if (e['err_code'] != 0) {
-    //     return Future.error(PlayDetailException('获取歌词失败'));
-    //   }
-    //   log('歌词=${e['data']['lyrics']}');
-    //   return Future.value(e['data']['lyrics']);
-    // });
+    return _doRequest(
+            "https://www.kuwo.cn/openapi/v1/www/lyric/getlyric?musicId=${track.id}&httpsStatus=1",
+            {},
+            {},
+            'get')
+        .then((value) => value.transform(utf8.decoder).join())
+        .then((value) => json.decode(value))
+        .then((value) {
+      if (value['code'] != 200) {
+        return Future.error(LyricException('获取歌词失败'));
+      }
+/*
+{
+    "code": 200,
+    "msg": "success",
+    "reqId": "b53df35a992fb8c299ac0338fb2f654d",
+    "data": {
+        "lrclist": [
+            {
+                "lineLyric": "Hello - Adele",
+                "time": "268.44"
+            }
+        ]
+    },
+    "profileId": "site",
+    "curTime": 1731403015553,
+    "success": true
+}*/
+
+      return Future.value(
+          LyricContent.from((value['data']['lrclist'] as List).map((v) {
+        return '[' +
+            _formatDuration(Duration(
+                milliseconds: (double.parse(v['time']) * 1000).toInt())) +
+            ']' +
+            v['lineLyric'].toString();
+      }).join("\n")));
+    });
+  }
+
+  Future<HttpClientResponse> _doRequest(
+      String url, Map<String, String> headers, Map data, String method) {
+    return HttpClient().openUrl(method, Uri.parse(url)).then((request) {
+      headers.forEach(request.headers.add);
+      if (cookie != null) {
+        request.headers.add("Cookie", COOKIE_NAME + "=" + cookie!);
+      }
+      request.write(Uri(queryParameters: data.cast()).query);
+      return request.close();
+    }).then((res) {
+      // 保存cookie
+      cookie = res.cookies.where((c) => c.name == COOKIE_NAME).map((c) {
+        return c.value;
+      }).join("; ");
+      return res;
+    });
+  }
+
+  String _getV(int l, String v) {
+    if (l >= v.length) {
+      return "";
+    }
+    return v[l];
+  }
+
+  BigInt _parseBigInt(String v) {
+// 模拟 js parseInt 效果，当v中出现非数字，忽略后面所有值
+    String m = v;
+    for (var i = 0; i < v.length; i++) {
+      var h = (v.codeUnitAt(i));
+      if (h < 48 || h >= 58) {
+        m = v.substring(0, i);
+        break;
+      }
+    }
+    return BigInt.parse(m);
+  }
+
+  String _bigIntToString(BigInt v) {
+// 模拟 js toString 效果，当数字长度超过21时，使用科学计数法
+    var s = v.toString();
+    var out = v.toString();
+    if (s.length > 21) {
+      out = s[0] + "." + s.substring(1, min(16, s.length));
+      if (s.length > out.length) {
+        // 四舍五入
+        if (s.codeUnitAt(out.length) >= 53) {
+          // 最后一位 + 1
+          out += (int.parse(s[out.length - 1]) + 1).toString();
+        } else {
+          out += s[out.length - 1];
+        }
+      }
+
+      out += "e+" + (s.length - 1).toString();
+    }
+
+    return out;
+  }
+
+  String _getSecret(String value, String name) {
+    // 获取 unicode 编码，因为n都在ascii内，所以就是获取ascii码
+    String n = "";
+    for (var i = 0; i < name.length; i++) {
+      n += name.codeUnitAt(i).toString();
+    }
+    var o = (n.length / 5).floor();
+    var r = int.parse(_getV(o, n) +
+        _getV(2 * o, n) +
+        _getV(3 * o, n) +
+        _getV(4 * o, n) +
+        _getV(5 * o, n));
+    var c = (name.length / 2).ceil();
+    var l = pow(2, 31) - 1;
+    if (r < 2) return "";
+    var d = ((1e9 * Random().nextDouble()).round() % 1e8).toInt();
+    for (n += d.toString(); n.length > 10;) {
+      n = _bigIntToString(_parseBigInt(n.substring(0, 10)) +
+          _parseBigInt(n.substring(10, n.length)));
+    }
+    var nn = (r * int.parse(n) + c) % l;
+    var f = 1;
+    var h = "";
+    for (var i = 0; i < value.length; i++) {
+      f = value.codeUnitAt(i) ^ (nn / l * 255).floor();
+      h += f < 16 ? ("0" + f.toRadixString(16)) : f.toRadixString(16);
+      nn = (r * nn + c) % l;
+    }
+    var nd = d.toRadixString(16);
+    for (; nd.length < 8;) {
+      nd = "0" + nd;
+    }
+    return h + nd;
   }
 }
 
-Future<HttpClientResponse> _doRequest(
-    String url, Map<String, String> headers, Map data, String method) {
-  return HttpClient().openUrl(method, Uri.parse(url)).then((request) {
-    headers.forEach(request.headers.add);
-    log('query=${Uri(queryParameters: data.cast()).query}');
-    request.write(Uri(queryParameters: data.cast()).query);
-    return request.close();
-  });
-}
